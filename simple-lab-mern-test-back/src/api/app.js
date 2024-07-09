46d20d0c8d8b4129f80deb172c5caf36df0eac02
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2024-07-08 23:19:06 -0300

    fix: enable cors

--- simple-lab-mern-test-back/src/api/app.js
+++ simple-lab-mern-test-back/src/api/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import usersController from "./controllers/users.controller.js";
 import UserRepository from "../infrastructure/repository/mongodb/user.repository.js";
 import UserService from "../domain/service/user.service.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
